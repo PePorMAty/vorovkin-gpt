a9504384b188a7d168bc2425d69b6ba7ef58701c
pepormaty <pepormaty@gmail.com>
2025-10-07 16:49:34 +0700

    demo gpt

--- src/make-request.tsx
+++ src/make-request.tsx
@@ -1,16 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { useAppDispatch, useAppSelector } from "./store/store";
+import React, { useState } from "react";
+import { useAppDispatch } from "./store/store";
 import { gptRequest } from "./store/slices/gpt/gpt-slice";
 
 export const MakeRequest = () => {
   const [value, setValue] = useState("");
-  const [gptRes, setGptRes] = useState([]);
   const dispatch = useAppDispatch();
-  const { data } = useAppSelector((state) => state.gpt);
-  console.log(data);
-  useEffect(() => {
-    setGptRes(data);
-  }, [data, gptRes]);
 
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
@@ -28,9 +22,6 @@ export const MakeRequest = () => {
     <div>
       <input type="text" value={value} onChange={(e) => handleOnChange(e)} />
       <button onClick={handleOnClick}>Сгенерировать</button>
-      {/* {data.nodes.map((elem: any) => {
-        <div>{elem}</div>;
-      })} */}
     </div>
   );
 };
